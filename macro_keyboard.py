2443ebf4060a00bf1117adf8b8063e9973d645fa
Constantin-Alexandru <constantinalexandruaurel@yahoo.com>
2023-05-14 23:51:33 +0100

    Added comments

--- macro_keyboard.py
+++ macro_keyboard.py
@@ -1,3 +1,15 @@
+'''
+Author1: Alexandru Constantin
+Author2: <Insert Name Here>
+
+Date: May 2023
+
+REQUIREMENTS: 
+* Adafruit_CircuitPython_HID: https://github.com/adafruit/Adafruit_CircuitPython_HID
+
+The following program runs on the Raspberry Pi Pico and allows the mapping of keyboard shortcuts to a single button on the Maker Pi Pico.
+'''
+
 import time
 import board
 import usb_hid
@@ -5,10 +17,12 @@ import digitalio
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
 
+# Saving the PINS of the 3 buttons
 btn1_pin = board.GP20
 btn2_pin = board.GP21
 btn3_pin = board.GP22
 
+# Setting up the buttons
 btn1 = digitalio.DigitalInOut(btn1_pin)
 btn1.direction = digitalio.Direction.INPUT
 
@@ -18,15 +32,22 @@ btn2.direction = digitalio.Direction.INPUT
 btn3 = digitalio.DigitalInOut(btn3_pin)
 btn3.direction = digitalio.Direction.INPUT
 
+# Setting up the keyboard
 keyboard = Keyboard(usb_hid.devices)
 
 
 while True:
     if not btn1.value:
         # Add code for mapping the 1st button
+        # There needs to be a press and a release action for each key.
+        # Make sure to add a small delay between the two to ensure that both actions are registered
     if not btn2.value:
         # Add code for mapping the 2nd button
+        # There needs to be a press and a release action for each key.
+        # Make sure to add a small delay between the two to ensure that both actions are registered
     if not btn3.value:
         # Add code for mapping the 3rd button
+        # There needs to be a press and a release action for each key.
+        # Make sure to add a small delay between the two to ensure that both actions are registered
         
     time.sleep(0.1)
